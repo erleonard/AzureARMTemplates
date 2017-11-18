8ccee8665d6ec937e8d58e89ff23e6636644605d
erleonard <erleonard@outlook.com>
2017-11-18 11:56:28 -0500

    update

---
+++ AzureDTL/Artifacts/sample-app/installSampleApp.ps1
@@ -0,0 +1,40 @@
+﻿Function Get-RedirectedUrl
+{
+    Param (
+        [Parameter(Mandatory=$true)]
+        [String]$URL
+    )
+ 
+    $request = [System.Net.WebRequest]::Create($url)
+    $request.AllowAutoRedirect=$false
+    $response=$request.GetResponse()
+ 
+    If ($response.StatusCode -eq "Found")
+    {
+        $response.GetResponseHeader("Location")
+    }
+}
+
+$url = 'https://go.microsoft.com/fwlink/?Linkid=852155'
+$codeSetupUrl = Get-RedirectedUrl -URL $url
+
+$infPath = $PSScriptRoot + "\vscode.inf"
+$vscodeSetup = "${env:Temp}\VSCodeSetup.exe"
+
+try
+{
+    (New-Object System.Net.WebClient).DownloadFile($codeSetupUrl, $vscodeSetup)
+}
+catch
+{
+    Write-Error "Failed to download VSCode Setup"
+}
+
+try
+{
+    Start-Process -FilePath $vscodeSetup -ArgumentList "/VERYSILENT /MERGETASKS=!runcode"
+}
+catch
+{
+    Write-Error 'Failed to install VSCode'
+}
